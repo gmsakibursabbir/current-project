80a8283ef2a345f10169890e4f3dc2aea3d82b5d
Sakibur Rahman Sabbir <106433632+gmsakibursabbir@users.noreply.github.com>
2024-03-09 23:27:22 +0600

    update

--- js/main.js
+++ js/main.js
@@ -1,48 +1,19 @@
-// Params
-var sliderSelector = '.swiper-container',
-    options = {
-      init: false,
-      loop: true,
-      speed:800,
-      slidesPerView: 4, // or 'auto'
-      // spaceBetween: 10,
-      centeredSlides : true,
-      effect: 'coverflow', // 'cube', 'fade', 'coverflow',
-      coverflowEffect: {
-        rotate: 0, // Slide rotate in degrees
-        stretch: 0, // Stretch space between slides (in px)
-        depth: 100, // Depth offset in px (slides translate in Z axis)
-        modifier: 2, // Effect multipler
-        slideShadows : true, // Enables slides shadows
-      },
-      grabCursor: true,
-      parallax: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        1180: {
-          slidesPerView: 2
-        },
-        1023: {
-          slidesPerView: 1,
-          spaceBetween: 0
-        }
-      },
-      // Events
-      on: {
-        imagesReady: function(){
-          this.el.classList.remove('loading');
-        }
-      }
-    };
-var mySwiper = new Swiper(sliderSelector, options);
+$(document).ready(function() {
+  // Assign some jquery elements we'll need
+  var $swiper = $(".swiper-container");
+  var $bottomSlide = null; 
+  var $bottomSlideContent = null;
 
-// Initialize slider
-mySwiper.init();
-
+  var mySwiper = new Swiper(".swiper-container", {
+    spaceBetween: 1,
+    slidesPerView: 3,
+    centeredSlides: true,
+    roundLengths: true,
+    loop: true,
+    loopAdditionalSlides: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+});
